0ef75030c8947fda36e6eddfa0f5826ca804c1db
salah <salahahlaweey8@gmail.com>
2024-04-15 22:04:40 +0200

    comment

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+
 app.use(showMethod);  //Show method
 app.use("/auth", Auth);
 app.use(verifyToken); //Verifing JWT
