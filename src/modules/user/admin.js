f50b638c2e77ee30a02a3aa1c6d03567d77d6532
Ahmed <Ahmed10mahmud29@gmail.com>
2024-06-04 15:51:23 +0300

    add get user by userName

--- src/modules/user/admin.js
+++ src/modules/user/admin.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { allUsers, deleteUser } from './controller/admincontroller.js';
+import { allUsers, deleteUser, getUserByUserName } from './controller/admincontroller.js';
 import { verifyRoles } from '../../middlewares/verifyroles.js';
 import ROLES_LIST from '../../../config/roleslist.js';
 
 //Delete user 
 router.delete('/delete', verifyRoles(ROLES_LIST.Admin), deleteUser);
 router.get('/allUsers', verifyRoles(ROLES_LIST.Admin), allUsers);
+router.get('/:userName',verifyRoles(ROLES_LIST.Admin) ,getUserByUserName);
 
 export default router
